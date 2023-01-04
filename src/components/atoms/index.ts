0776d31eac6b6df1bf67c0e509f7365eac6b0827
DaveTheWebDev <dawidantczak2@gmail.com>
2023-01-04 09:10:06 +0100

    refactor: atoms and molecules import

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,9 @@
+import { Accordion } from "components/atoms/Accordion/Accordion";
+import { Button } from "components/atoms/Button/Button";
+import { Checkbox } from "components/atoms/Checkbox/Checkbox";
+import { ChosenPrivileges } from "components/atoms/ChosenPrivileges/ChosenPrivileges";
+import { InfoSection } from "components/atoms/InfoSection/InfoSection";
+import { SelectedCompany } from "components/atoms/SelectedCompany/SelectedCompany";
+import { Step } from "components/atoms/Step/Step";
+
+export { Accordion, Button, Checkbox, ChosenPrivileges, InfoSection, SelectedCompany, Step };
---
+++ src/components/molecules/index.ts
@@ -0,0 +1,6 @@
+import { CheckboxList } from "./CheckboxList/CheckboxList";
+import { Overview } from "./Overview/Overview";
+import { PrivilegesList } from "./PrivilegesList/PrivilegesList";
+import { Privileges } from "./Privileges/Privileges";
+
+export { CheckboxList, Overview, Privileges, PrivilegesList };
