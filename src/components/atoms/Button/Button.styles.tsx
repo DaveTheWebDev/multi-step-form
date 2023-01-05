ef9f6fbce55c7e1d3585186028c362b21fd8c7b7
DaveTheWebDev <dawidantczak2@gmail.com>
2023-01-05 08:42:20 +0100

    chore: added buttons hover effect

--- src/components/atoms/Button/Button.styles.tsx
+++ src/components/atoms/Button/Button.styles.tsx
@@ -7,12 +7,25 @@ const primary = css`
 
   color: ${({ theme }) => theme.colors.white};
   background-color: ${({ theme }) => theme.colors.primary};
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  
+  transition: .3s;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.primary};
+    background-color: ${({ theme }) => theme.colors.white};
+  }
   
 `;
 const dangerous = css`
   color: ${({ theme }) => theme.colors.error};
 
-  text-decoration: underline;
+  transition: .3s;
+
+  &:hover {
+    text-decoration: underline;
+
+  }
 `;
 
 const Button = styled.button<ButtonProps>`
@@ -25,13 +38,13 @@ const Button = styled.button<ButtonProps>`
 
   cursor: pointer;
   ${({ variant }) => {
-		switch (variant) {
-		case "primary":
-			return primary;
-		case "dangerous":
-			return dangerous;
-		}
-	}}
+    switch (variant) {
+      case "primary":
+        return primary;
+      case "dangerous":
+        return dangerous;
+    }
+  }}
 `;
 
 export { Button };
